cea00f43dc4b5e32af97b5fd863c06962746888a
Sajjad <shahbazi.developer@gmail.com>
2023-11-05 21:49:45 +0330

    change path repository

--- data/src/main/java/sajjad/shahbazi/data/remote/UserRemoteApi.kt
+++ data/src/main/java/sajjad/shahbazi/data/remote/UserRemoteApi.kt
@@ -7,10 +7,10 @@ import sajjad.shahbazi.data.models.UserServerModel
 import sajjad.shahbazi.data.models.RetrofitResult
 
 interface UserRemoteApi {
-    @GET("v3/4cb4fcb1-0fc0-466e-9048-044c2cf0d4f7")
+    @GET("v3/8ec4b8e2-59ca-460d-bdd6-27e4f3f61dfe")
     suspend fun getUsers(): RetrofitResult<List<UserServerModel>>
 
-    @GET("v3/c797f123-505b-4190-9d46-67848859f72f")
+    @GET("v3/6022922f-d17b-4458-9e4d-bb29ff5b505d")
     suspend fun getUser(): RetrofitResult<UserServerModel>
 
     companion object {
